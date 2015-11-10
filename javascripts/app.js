27d25dde86bdca79deffcea4bcbdaff43be04bf5
makafis <vinnyalfieri@gmail.com>
2015-11-10 10:20:50 -0500

    board created

---
+++ javascripts/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var space = 1;
+  for (var r=0; r<8; r++) {
+    var col = "";
+    for (var c=0; c<8; c++) { col += "<td data-pos='"+space+"'></td>"; space++; }
+    $("#chessboard").append("<tr>"+col+"</tr>");
+  };
+});
